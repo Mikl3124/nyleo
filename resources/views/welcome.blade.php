66a93aaf1bc404f292c0d6db5474a7eb7410a156
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-12 23:19:47 +0200

    travis configuration v_10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    AWS Integration https v_2
+                    AWS Integration https v_3
                 </div>
 
                 <div class="links">
