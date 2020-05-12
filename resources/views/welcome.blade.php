39fabecb7e4937fedcb88b7bb1731eea834f2bab
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-13 00:16:15 +0200

    https configuration v_2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    4....
+                    5....
                 </div>
 
                 <div class="links">
