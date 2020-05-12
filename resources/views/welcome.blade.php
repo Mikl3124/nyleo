d691ec5db5129124f7021a349fbfac7de2086fc9
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-13 01:32:16 +0200

    https confi 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    11....
+                    12....
                 </div>
 
                 <div class="links">
