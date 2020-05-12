674b309fec9ff6105bcf5a65ac22cde3e715d27d
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-13 00:04:01 +0200

    travis configuration v_15

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    1....
+                    2....
                 </div>
 
                 <div class="links">
