9fce9b64a9df6d7ca04bb591547261f3cdf415dd
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-13 21:18:04 +0200

    new version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Nyleo!
+                    Hello Client !
                 </div>
 
                 <div class="links">
