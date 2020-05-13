5f2011ffdf74a6a7e0bf15679af1d98e05f6805a
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-13 21:20:22 +0200

    new version 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Client !
+                    Hello Client 2 !
                 </div>
 
                 <div class="links">
