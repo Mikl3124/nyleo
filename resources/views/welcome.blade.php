74cc2900d83bd62bed8a38243891aa23cd9316f1
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-13 19:12:40 +0200

    https v_4_1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Nyleo !
+                    Hello Nyleo 2 !
                 </div>
 
                 <div class="links">
