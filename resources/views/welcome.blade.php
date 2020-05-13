234a165c61e98cd1d22163b8da6519aecf1bd3e9
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-13 14:26:32 +0200

    https v3_10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Https....v8
+                    Https....v9
                 </div>
 
                 <div class="links">
