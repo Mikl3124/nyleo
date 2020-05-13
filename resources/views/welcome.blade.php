018771c106a781d3c56881f1846a60cda4bd55ac
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-13 15:41:29 +0200

    https v3_16

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Helloooool
+                    Yesss
                 </div>
 
                 <div class="links">
