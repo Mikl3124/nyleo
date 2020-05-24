e59de045600c054d62e3fddd38bfb0de41f591a1
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-24 05:00:33 +0200

    "update"

--- resources/views/client/projet-form.blade.php
+++ resources/views/client/projet-form.blade.php
@@ -150,7 +150,7 @@
         <label class="label" for="file5">
         </label>
         <div class="file-uploader__message-area">
-          <p>Select the file</p>
+          <p>Select file</p>
         </div>
         <div class="file-chooser">
           <input type="file" class="file-chooser__input" id="file5" name="file5[]">
