64df8a12c44718f3b721a52fe540dee0ecbad6db
Mickael Delpech <mickael.delpech@gmail.com>
2020-10-11 21:04:22 +0200

    added dessinateur btn

--- resources/views/admin/clients/show.blade.php
+++ resources/views/admin/clients/show.blade.php
@@ -12,6 +12,32 @@
     <a href="{{ route('admin.upload.page', $user) }}" class="btn btn-primary">Envoyer un document</a>
     <a href="{{ route('devis.create', $user) }}" class="btn btn-primary">Devis</a>
     <a href="{{ route('avantProjet.create', $user) }}" class="btn btn-primary">Envoyer l'avant projet</a>
+    <div class="text-right">
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
+        Assigner un dessinateur
+      </button>
+    </div>
+    
   </div>
 
 @endsection
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Liste des dessinateurs</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,6 +6,9 @@
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#CreateUserModal">
       Créer un client
     </button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#CreateDessinateurModal">
+      Créer un dessinateur
+    </button>
     <form action="{{ route('test.mail') }}" method="POST">
       @csrf
       <button type="submit" class="btn btn-success mt-3">Test Mail</button>
@@ -54,6 +57,30 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="CreateUserModalLabel">Créer un utilisateur</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="{{ route('client.store') }}">
+            @csrf
+            <div class="form-group">
+                <input placeholder="Adresse e-mail" id="email" type="email" class="form-control" name="email" required autocomplete="email">
+            </div>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+            <button type="submit" class="btn btn-success">Créer</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal Création Dessinateur-->
+<div class="modal fade" id="CreateDessinateurModal" tabindex="-1" role="dialog" aria-labelledby="CreateDessinateurModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="CreateDessinateurModal">Créer un dessinateur</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
