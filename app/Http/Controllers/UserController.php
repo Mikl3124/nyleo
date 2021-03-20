f2c0d2b0f0cc2b48d97c193c414ea9414850f1be
Mickael Delpech <mickael.delpech@gmail.com>
2021-03-20 19:23:23 +0100

    changed email step 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Validator;
 use App\Model\User;
 use App\Mail\TestMail;
 use App\Jobs\JobTestMail;
@@ -13,141 +12,134 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Jobs\MailWelcomeMessageToUser;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 
 class UserController extends Controller
 {
   public function clientShow()
-    {
-      $step = Auth::user()->step;
-      return view('client.client-show', compact('step'));
-    }
+  {
+    $step = Auth::user()->step;
+    return view('client.client-show', compact('step'));
+  }
 
   public function store(Request $request)
-    {
-      $user = New User;
-      $value = $request->all();
+  {
+    $user = new User;
+    $value = $request->all();
 
-        $rules = [
-            'email' => 'required'
-        ];
+    $rules = [
+      'email' => 'required'
+    ];
 
-        $validator = Validator::make($value, $rules,[
+    $validator = Validator::make($value, $rules, []);
 
-          ]);
-
-          if($validator->fails()){
-            return Redirect::back()
-              ->withErrors($validator)
-              ->withInput();
-          } else{
-            $user->email = $request['email'];
-            $user->password = Hash::make('nyleo');
-            $user->role = 'client';
-            $user->save();
-            //Send mail to new client
-            $this->dispatch(new MailWelcomeMessageToUser($user));
-            return Redirect::back();
-          }
+    if ($validator->fails()) {
+      return Redirect::back()
+        ->withErrors($validator)
+        ->withInput();
+    } else {
+      $user->email = $request['email'];
+      $user->password = Hash::make('nyleo');
+      $user->role = 'client';
+      $user->save();
+      //Send mail to new client
+      $this->dispatch(new MailWelcomeMessageToUser($user));
+      return Redirect::back();
     }
+  }
 
-    public function changePassword(Request $request)
-    {
-      $user = Auth::user();
-      $step = 0;
-      $value = $request->all();
+  public function changePassword(Request $request)
+  {
+    $user = Auth::user();
+    $step = 0;
+    $value = $request->all();
 
-        $rules = [
-            'password' => ['confirmed'],
-        ];
+    $rules = [
+      'password' => ['confirmed'],
+    ];
 
-        $validator = Validator::make($value, $rules,[
-            'password.confirmed' => 'Les mots de passe ne sont pas identiques',
+    $validator = Validator::make($value, $rules, [
+      'password.confirmed' => 'Les mots de passe ne sont pas identiques',
 
-          ]);
+    ]);
 
-          if($validator->fails()){
-            return Redirect::back()
-              ->withErrors($validator)
-              ->withInput();
-          } else{
-            $user->password = Hash::make($request['password']);
-            $user->custom_password = true;
-            if($user->save()){
-              return view('client.dashboard', compact('step'));
-            }
-            return Redirect::back();
-          }
+    if ($validator->fails()) {
+      return Redirect::back()
+        ->withErrors($validator)
+        ->withInput();
+    } else {
+      $user->password = Hash::make($request['password']);
+      $user->custom_password = true;
+      if ($user->save()) {
+        return view('client.dashboard', compact('step'));
+      }
+      return Redirect::back();
     }
+  }
 
-    public function clientEdit()
-    {
-      $user = Auth::user();
-        $step = $user->step;
-        return view('client.client-form', compact('user', 'step'));
+  public function clientEdit()
+  {
+    $user = Auth::user();
+    $step = $user->step;
+    return view('client.client-form', compact('user', 'step'));
+  }
+
+  public function testMail()
+  {
+    $user = Auth::user();
+    //Mail::to($user->email)->send(new TestMail($user));
+    $this->dispatch(new JobTestMail($user));
+    return Redirect::back();
+  }
+
+
+  public function clientUpdate(Request $request, $user)
+  {
+
+    $user = User::find($user);
+    $value = $request->all();
+    $rules = [
+      'lastname' => 'required',
+      'firstname' => 'required',
+      'birth' => 'required',
+      'birthplace' => 'required',
+      'address' => 'required',
+      'town' => 'required',
+      'cp' => 'required | numeric',
+      'email' => 'required | email',
+      'phone' => 'required'
+    ];
+
+    $validator = Validator::make($value, $rules, []);
+
+    if ($validator->fails()) {
+      return Redirect::back()
+        ->withErrors($validator)
+        ->withInput();
+    } else {
+      $user->email = $request['email'];
+      $user->lastname = $request->lastname;
+      $user->firstname = $request->firstname;
+      $user->birth = $request->birth;
+      $user->birthplace = $request->birthplace;
+      $user->address = $request->address;
+      $user->town = $request->town;
+      $user->cp = $request->cp;
+      $user->email = $request->email;
+      $user->phone = $request->phone;
+
+      if ($user->save()) {
+        if ($user->step < 1) {
+          $user->step = 1;
+        }
+        $user->save();
+        //Send mail to new client
+        $step = Auth::user()->step;
+        $this->dispatch(new MailWelcomeMessageToUser($user));
+        return view('client.dashboard', compact('step'));
+      };
+      return Redirect::back();
     }
-
-    public function testMail()
-    {
-      $user = Auth::user();
-        //Mail::to($user->email)->send(new TestMail($user));
-        $this->dispatch(new JobTestMail($user));
-        return Redirect::back();
-
-    }
-
-
-    public function clientUpdate(Request $request, $user)
-    {
-
-      $user = User::find($user);
-      $value = $request->all();
-      $rules = [
-            'lastname' => 'required',
-            'firstname' => 'required',
-            'birth' => 'required',
-            'birthplace' => 'required',
-            'address' => 'required',
-            'town' => 'required',
-            'cp' => 'required | numeric',
-            'email' => 'required | email',
-            'phone' => 'required'
-        ];
-
-        $validator = Validator::make($value, $rules,[
-
-          ]);
-
-          if($validator->fails()){
-            return Redirect::back()
-              ->withErrors($validator)
-              ->withInput();
-            } else{
-              $user->email = $request['email'];
-              $user->lastname = $request->lastname;
-              $user->firstname = $request->firstname;
-              $user->birth = $request->birth;
-              $user->birthplace = $request->birthplace;
-              $user->address = $request->address;
-              $user->town = $request->town;
-              $user->cp = $request->cp;
-              $user->email = $request->email;
-              $user->phone = $request->phone;
-
-              if($user->save()){
-                if ($user->step < 1){
-                  $user->step = 1;
-                }
-                $user->save();
-                //Send mail to new client
-                $step = Auth::user()->step;
-                $this->dispatch(new MailWelcomeMessageToUser($user));
-                return view('client.dashboard', compact('step'));
-              };
-            return Redirect::back();
-
-          }
-
-    }
-
+  }
 }
--- resources/views/emails/welcome-message-to-client.blade.php
+++ resources/views/emails/welcome-message-to-client.blade.php
@@ -1,58 +1,402 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Un nouveau projet a été posté</title>
+    <style>
+      /* -------------------------------------
+          GLOBAL RESETS
+      ------------------------------------- */
 
-<head>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&amp;display=swap" rel="stylesheet">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-</head>
+      /*All the styling goes here*/
 
-<body style="font-family: 'Roboto', sans-serif;color:#353b48;">
-    <span class="preheader">Votre accès personnalisé à Nyleo</span>
-    <table with="100%" style="max-width:650px;display:block;margin:auto;">
-        <tr>
-          <td>
+      img {
+        border: none;
+        -ms-interpolation-mode: bicubic;
+        max-width: 100%;
+      }
 
-          </td>
-        </tr>
-        <tr style="text-align:center">
-            <td width="100%">
-                <img style="height:80px" src="https://nyleo.s3.eu-west-3.amazonaws.com/Logo-nyleoxhdpi.png" alt="logo iziplans">
-            </td>
-        </tr>
-        <tr>
-          <td width="100%" style="font-size:22px;display: block;margin-bottom:30px;">Bonjour</td>
-        </tr>
-        <tr>
-            <td width="100%" style="font-size:22px;display: block;margin-bottom:30px;">Mickael vient de créer votre accès membre sur la plateforme de communication "Nyleo"</td>
-        </tr>
-        <tr>
-            <td width="100%" style="font-size:22px;display: block;margin-bottom:30px;text-decoration: underline;">Voici vos identifiants:</td>
-        </tr>
-        <tr>
-            <td width="100%" style="font-size:22px;display: block;margin-bottom:30px;">Email: {{ $user->email }}</td>
+      body {
+        background-color: #f6f6f6;
+        font-family: sans-serif;
+        -webkit-font-smoothing: antialiased;
+        font-size: 14px;
+        line-height: 1.4;
+        margin: 0;
+        padding: 0;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+      }
 
-        </tr>
-        <tr>
-            <td width="50%" style="font-size:22px;display: block;margin-bottom:30px;">Mot de passe provisoire: nyleo</td>
-        </tr>
-        <tr>
-            <td width="100%" style="font-size:22px;display: block;margin-bottom:30px;"></td>
-        </tr>
+      table {
+        border-collapse: separate;
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+        width: 100%; }
+        table td {
+          font-family: sans-serif;
+          font-size: 14px;
+          vertical-align: top;
+      }
 
-        <tr style="text-align:center">
-            <td align="center" bgcolor="#ff6600" role="presentation" style="border:none;border-radius:5px;background:#ffff" valign="middle">
+      /* -------------------------------------
+          BODY & CONTAINER
+      ------------------------------------- */
 
-            <a href="https://clients.nyleo.fr" style="display:inline-block;background:#ff6600;color:#ffffff;font-family:Roboto,sans-serif;font-size:15px;font-weight:normal;line-height:120%;margin:0;text-decoration:none;text-transform:none;padding:16px 20px;border-radius:5px" target="_blank">
+      .body {
+        background-color: #f6f6f6;
+        width: 100%;
+      }
 
-              ACCÉDER A LA PLATEFORME
+      /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
+      .container {
+        display: block;
+        margin: 0 auto !important;
+        /* makes it centered */
+        max-width: 580px;
+        padding: 10px;
+        width: 580px;
+      }
 
-            </a>
+      /* This should also be a block element, so that it will fill 100% of the .container */
+      .content {
+        box-sizing: border-box;
+        display: block;
+        margin: 0 auto;
+        max-width: 580px;
+        padding: 10px;
+      }
 
-          </td>
-          </tr>
+      /* -------------------------------------
+          HEADER, FOOTER, MAIN
+      ------------------------------------- */
+      .main {
+        background: #ffffff;
+        border-radius: 3px;
+        width: 100%;
+      }
+
+      .wrapper {
+        box-sizing: border-box;
+        padding: 20px;
+      }
+
+      .content-block {
+        padding-bottom: 10px;
+        padding-top: 10px;
+      }
+
+      .footer {
+        clear: both;
+        margin-top: 10px;
+        text-align: center;
+        width: 100%;
+      }
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+          color: #999999;
+          font-size: 12px;
+          text-align: center;
+      }
+
+      /* -------------------------------------
+          TYPOGRAPHY
+      ------------------------------------- */
+      h1,
+      h2,
+      h3,
+      h4 {
+        color: #000000;
+        font-family: sans-serif;
+        font-weight: 400;
+        line-height: 1.4;
+        margin: 0;
+        margin-bottom: 30px;
+      }
+
+      h1 {
+        font-size: 35px;
+        font-weight: 300;
+        text-align: center;
+        text-transform: capitalize;
+      }
+
+      p,
+      ul,
+      ol {
+        font-family: sans-serif;
+        font-size: 14px;
+        font-weight: normal;
+        margin: 0;
+        margin-bottom: 15px;
+      }
+        p li,
+        ul li,
+        ol li {
+          list-style-position: inside;
+          margin-left: 5px;
+      }
+
+      a {
+        color: #ff6600;
+        text-decoration: underline;
+      }
+
+      /* -------------------------------------
+          BUTTONS
+      ------------------------------------- */
+      .btn {
+        box-sizing: border-box;
+        width: 100%; }
+        .btn > tbody > tr > td {
+          padding-bottom: 15px; }
+        .btn table {
+          width: auto;
+      }
+        .btn table td {
+          background-color: #ffffff;
+          border-radius: 5px;
+          text-align: center;
+      }
+        .btn a {
+          background-color: #ffffff;
+          border: solid 1px #ff6600;
+          border-radius: 5px;
+          box-sizing: border-box;
+          color: #ff6600;
+          cursor: pointer;
+          display: inline-block;
+          font-size: 14px;
+          font-weight: bold;
+          margin: 0;
+          padding: 12px 25px;
+          text-decoration: none;
+          text-transform: capitalize;
+      }
+
+      .btn-primary table td {
+        background-color: #ff6600;
+      }
+
+      .btn-primary a {
+        background-color: #ff6600;
+        border-color: #ff6600;
+        color: #ffffff;
+      }
+
+      /* -------------------------------------
+          OTHER STYLES THAT MIGHT BE USEFUL
+      ------------------------------------- */
+      .last {
+        margin-bottom: 0;
+      }
+
+      .first {
+        margin-top: 0;
+      }
+
+      .align-center {
+        text-align: center;
+      }
+
+      .align-right {
+        text-align: right;
+      }
+
+      .align-left {
+        text-align: left;
+      }
+
+      .clear {
+        clear: both;
+      }
+
+      .mt0 {
+        margin-top: 0;
+      }
+
+      .mb0 {
+        margin-bottom: 0;
+      }
+
+      .preheader {
+        color: transparent;
+        display: none;
+        height: 0;
+        max-height: 0;
+        max-width: 0;
+        opacity: 0;
+        overflow: hidden;
+        mso-hide: all;
+        visibility: hidden;
+        width: 0;
+      }
+
+      .powered-by a {
+        text-decoration: none;
+      }
+
+      hr {
+        border: 0;
+        border-bottom: 1px solid #f6f6f6;
+        margin: 20px 0;
+      }
+
+      /* -------------------------------------
+          RESPONSIVE AND MOBILE FRIENDLY STYLES
+      ------------------------------------- */
+      @media only screen and (max-width: 620px) {
+        table[class=body] h1 {
+          font-size: 28px !important;
+          margin-bottom: 10px !important;
+        }
+        table[class=body] p,
+        table[class=body] ul,
+        table[class=body] ol,
+        table[class=body] td,
+        table[class=body] span,
+        table[class=body] a {
+          font-size: 16px !important;
+        }
+        table[class=body] .wrapper,
+        table[class=body] .article {
+          padding: 10px !important;
+        }
+        table[class=body] .content {
+          padding: 0 !important;
+        }
+        table[class=body] .container {
+          padding: 0 !important;
+          width: 100% !important;
+        }
+        table[class=body] .main {
+          border-left-width: 0 !important;
+          border-radius: 0 !important;
+          border-right-width: 0 !important;
+        }
+        table[class=body] .btn table {
+          width: 100% !important;
+        }
+        table[class=body] .btn a {
+          width: 100% !important;
+        }
+        table[class=body] .img-responsive {
+          height: auto !important;
+          max-width: 100% !important;
+          width: auto !important;
+        }
+      }
+
+      /* -------------------------------------
+          PRESERVE THESE STYLES IN THE HEAD
+      ------------------------------------- */
+      @media all {
+        .ExternalClass {
+          width: 100%;
+        }
+        .ExternalClass,
+        .ExternalClass p,
+        .ExternalClass span,
+        .ExternalClass font,
+        .ExternalClass td,
+        .ExternalClass div {
+          line-height: 100%;
+        }
+        .apple-link a {
+          color: inherit !important;
+          font-family: inherit !important;
+          font-size: inherit !important;
+          font-weight: inherit !important;
+          line-height: inherit !important;
+          text-decoration: none !important;
+        }
+        #MessageViewBody a {
+          color: inherit;
+          text-decoration: none;
+          font-size: inherit;
+          font-family: inherit;
+          font-weight: inherit;
+          line-height: inherit;
+        }
+        .btn-primary table td:hover {
+          background-color: #34495e !important;
+        }
+        .btn-primary a:hover {
+          background-color: #34495e !important;
+          border-color: #34495e !important;
+        }
+      }
+
+    </style>
+  </head>
+  <body class="">
+    <span class="preheader">Un nouveau projet a été posté sur iziplans</span>
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
+      <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+          <div class="content">
+            <!-- START CENTERED WHITE CONTAINER -->
+            <table role="presentation" class="main">
+              <tr style="text-align:center">
+                <td width="100%">
+                    <img style="height:50px" src="https://nyleo.s3.eu-west-3.amazonaws.com/Logo-nyleoxhdpi.png" alt="logo nyleo">
+                </td>
+            </tr>
+              <!-- START MAIN CONTENT AREA -->
+              <tr>
+                <td class="wrapper">
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                    <tr>
+                      <td>
+                        <p>Bonjour {{ $user->firstname }}</p>
+                        <p>Un nouveau projet a été posté, celui-ci est en relation avec vos compétences !</p>
+
+                        <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                          <tbody>
+                            <tr>
+                              <td align="left">
+                                <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                                  <tbody>
+                                    <tr>
+                                      <td> <a href="https://clients.nyleo.fr/" target="_blank">Aller sur nyleo</a> </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <p>A tout de suite </p>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+
+            <!-- END MAIN CONTENT AREA -->
+            </table>
+            <!-- END CENTERED WHITE CONTAINER -->
+
+            <!-- START FOOTER -->
+            <div class="footer">
+              <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td class="content-block powered-by">
+                    iziplans, vos plans facilement
+                  </td>
+                </tr>
+              </table>
+            </div>
+            <!-- END FOOTER -->
+
+          </div>
+        </td>
+        <td>&nbsp;</td>
+      </tr>
     </table>
-
-</body>
+  </body>
 </html>
